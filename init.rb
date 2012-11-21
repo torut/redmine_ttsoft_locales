60358f037b1654dcf42be10b9efb0c5be8ec9733
Toru Tamura <torut@ttsoft.jp>
2012-11-21 17:58:30 +0900

    init commit.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :ttsoft_locales do
+  name 'T.T.Soft Locales plugin'
+  author 'Toru Tamura(torut)'
+  description 'This is a override ja locale plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/torut/redmine_locales_plugin'
+  author_url 'http://ttsoft.jp/'
+end
